aa25d4dd08c7c90ae871c9f4d799b7998d04146d
jorge moreno <morenojorge308@gmail.com>
2022-03-04 13:12:19 -0600

    Primer cambio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Table,Button,Container,Modal,ModalBody,ModalHeader,FormGroup,ModalFooter} from "reactstrap";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component{
+  state={
+    data:data
+  }
+  render(){
+    return(
+      <>
+      <Container>
+        <Button color="primary">Insertar nueva carrera</Button>
+        <br/>
+<Table>
+    <thead>
+    <tr>
+    <th>Nombre de carrera</th>
+    <th>Correo electrónico</th>   
+    <th>Contraseña</th>
+    </tr>
+    </thead>
+    
+  <tbody>
+   {this.state.data.map((elemento)=>(
+     <tr>
+       <td>{elemento.nombre}</td>
+       <td>{elemento.correo}</td>
+       <td>{elemento.password}</td>
+       <td><Button color='primary'>Editar</Button></td>
+       <td><Button color='danger'>Eliminar</Button></td>
+     </tr>
+   ))}
+  </tbody>
+  </Table>
+      </Container>
+     
+      </>
 
+    )
+  }
+}
+const data=[
+  { nombre: 'Ingeniería Mecatrónica',correo:'mecatronica@rcarbonifera.tecnm.mx',password:'******'},
+  { nombre: 'Ingeniería en Sistemas Computacionales',correo:'sistemas@rcarbonifera.tecnm.mx',password:'******'},
+  { nombre: 'Ingeniería Electromecánica',correo:'electromecanica@rcarbonifera.tecnm.mx',password:'******'},
+  { nombre: 'Ingeniería Industrial',correo:'industrial@rcarbonifera.tecnm.mx',password:'******'},
+  { nombre: 'Ingeniería Petrolera',correo:'petrolera@rcarbonifera.tecnm.mx',password:'******'},
+  { nombre: 'Ingeniería en Administración',correo:'administracion@rcarbonifera.tecnm.mx',password:'******'},
+];
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,11 +6,39 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <>
+    <div className='marco'>
+   <div className='encabezado'>
+     <img src='http://187.216.2.135/tutorias/logo/itesrc.png'/>
+   <h1>Catálogo de carreras</h1>  
+   </div>
+   <div>
+   <App style="display:block;"/>
+     <FormAgregar style="display:block;" /><br/>
+ 
+   </div>
+
+   </div>  
+    </>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
 
+function FormAgregar(){
+  return (
+    <form >
+        
+      <label for="name">Nombre de la carrera:</label>
+      <input type="text" placeholder='Nombre de la carrera' name='name'  className="form-control" />
+      <label for="Correo Electrónico">Correo electrónico del coordinador:</label>
+      <input type="email" placeholder='ejemplo@hotmail.com' name='email' className="form-control"/>
+      <label for="password">Contraseña</label>
+      <input type="password" placeholder='Contraseña' name='password' className='form-control'></input>
+      <input type="submit" className="btn btn-success" ></input>
+   </form>
+  );
+  }
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
